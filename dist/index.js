9e079b5ecf00a97799f9ed2c7f0f3955ac1067a6
chinchilla <76946953+lewiiiis@users.noreply.github.com>
2022-01-24 18:24:53 +0900

    fix: name

--- dist/index.js
+++ dist/index.js
@@ -2,7 +2,7 @@
 'use strict'
 
 if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./restful-react.cjs.production.min.js')
+  module.exports = require('./react-query-openapi.cjs.production.min.js')
 } else {
-  module.exports = require('./restful-react.cjs.development.js')
+  module.exports = require('./react-query-openapi.cjs.development.js')
 }
